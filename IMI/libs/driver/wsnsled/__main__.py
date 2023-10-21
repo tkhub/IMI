7cc68dcb79ebb6f8173598b78c17c64966e19fcd
Imai <takap.colonel@gmail.com>
2023-10-21 16:13:27 +0900

    wall sensor led OK

---
+++ IMI/libs/driver/wsnsled/__main__.py
@@ -0,0 +1,63 @@
+import sys
+import time
+from wsnsled import WSNSLED
+
+def main():
+    WLED = WSNSLED()
+    state:int = 0
+    while True:
+        match state:
+            case 0:
+                WLED.write(WLED.LED_FL, WLED.WLED_OFF)
+                WLED.write(WLED.LED_FR, WLED.WLED_OFF)
+                WLED.write(WLED.LED_RR, WLED.WLED_OFF)
+                WLED.write(WLED.LED_LL, WLED.WLED_OFF)
+                print("OFF")
+                time.sleep(2)
+                state = 1
+
+            case 1:
+                WLED.write(WLED.LED_RR, WLED.WLED_ON)
+                WLED.write(WLED.LED_LL, WLED.WLED_OFF)
+                WLED.write(WLED.LED_FL, WLED.WLED_OFF)
+                WLED.write(WLED.LED_FR, WLED.WLED_OFF)
+                print("RR")
+                time.sleep(1)
+                state = 2
+
+            case 2:
+                WLED.write(WLED.LED_RR, WLED.WLED_OFF)
+                WLED.write(WLED.LED_LL, WLED.WLED_ON)
+                WLED.write(WLED.LED_FL, WLED.WLED_OFF)
+                WLED.write(WLED.LED_FR, WLED.WLED_OFF)
+                print("LL")
+                time.sleep(1)
+                state = 3
+
+            case 3:
+                WLED.write(WLED.LED_RR, WLED.WLED_OFF)
+                WLED.write(WLED.LED_LL, WLED.WLED_OFF)
+                WLED.write(WLED.LED_FL, WLED.WLED_ON)
+                WLED.write(WLED.LED_FR, WLED.WLED_OFF)
+                print("FL")
+                time.sleep(1)
+                state = 4
+
+            case 4:
+                WLED.write(WLED.LED_RR, WLED.WLED_OFF)
+                WLED.write(WLED.LED_LL, WLED.WLED_OFF)
+                WLED.write(WLED.LED_FL, WLED.WLED_OFF)
+                WLED.write(WLED.LED_FR, WLED.WLED_ON)
+                print("FR")
+                time.sleep(1)
+                state = 0
+            
+            case _:
+                state = 0
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt :
+        print("!!Exit!!")
+        sys.exit()
---
+++ IMI/libs/driver/wsnsled/wsnsled.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+
+class WSNSLED:
+    # LED3 RAS4 GPIO22
+    # LED2 RAS5 GPIO23
+    # LED1 RAS6 GPIO24
+    # LED0 RAS7 GPIO25
+    LED_RR = 22
+    LED_LL = 4
+    LED_FR = 27
+    LED_FL = 17
+    WLED_ON = True
+    WLED_OFF = False
+    def __init__(self) -> None:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.LED_RR, GPIO.OUT)
+        GPIO.setup(self.LED_LL, GPIO.OUT)
+        GPIO.setup(self.LED_FR, GPIO.OUT)
+        GPIO.setup(self.LED_FL, GPIO.OUT)
+    
+    def __del__(self):
+        GPIO.cleanup(self.LED_RR)
+        GPIO.cleanup(self.LED_LL)
+        GPIO.cleanup(self.LED_FR)
+        GPIO.cleanup(self.LED_FL)
+    
+    def close(self):
+        self.__del__()
+    
+    def write(self, ledch:int, level:bool) -> bool:
+        if level == self.WLED_ON:
+            GPIO.output(ledch, True)
+        else :
+            GPIO.output(ledch, False)
