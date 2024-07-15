acd06b53f30cca801399fb8f0d69aa6732da48f2
Imai <takap.colonel@gmail.com>
2024-07-15 20:27:34 +0900

    ・ディスプレイの表示が遅いのでディスプレイプロセスを作成 ・スイッチ全押しで停止

--- IMI_main.py
+++ IMI_main.py
@@ -9,6 +9,7 @@ from IMI.libs.devices.key import key as UIKey
 
 queue_man2machine = Queue()
 
+# センサ取得。センサからの値を取得して伝える。
 def sensorLoop(exiting):
     cnt = 0
     while not exiting.is_set():
@@ -17,32 +18,45 @@ def sensorLoop(exiting):
             cnt += 1
             sleep(0.1)
         except KeyboardInterrupt:
-            print("caught keyboardinterrupt")  # never caught here. just for demonstration purposes
+            print("sensor Loop caught keyboardinterrupt")  # never caught here. just for demonstration purposes
 
+
+# 走行制御。runCmdLoopからの司令や、sensorLoopからの情報を元に走行制御する
 def runCtrlLoop(exiting):
     cnt = 0
     while not exiting.is_set():
         try:
-            print(f"{clock_gettime(0)}: run cnt = {cnt}")
-            if not queue_man2machine.empty():
-                print(f"ui = {queue_man2machine.get_nowait()}")
+            print(f"{clock_gettime(0)}: run Ctrl cnt = {cnt}")
             cnt += 1
-            sleep(0.5)
+            sleep(0.2)
         except KeyboardInterrupt:
-            print("caught keyboardinterrupt")  # never caught here. just for demonstration purposes
+            print("Run Control Loop caught keyboardinterrupt")  # never caught here. just for demonstration purposes
 
+# 走行司令。SensorLoopやUIからの情報を元に何をするか決める
 def runCmdLoop(exiting):
     cnt = 0
     while not exiting.is_set():
         try:
-            print(f"{clock_gettime(0)}: run cnt = {cnt}")
+            print(f"{clock_gettime(0)}: run Cmd cnt = {cnt}")
             if not queue_man2machine.empty():
                 print(f"ui = {queue_man2machine.get_nowait()}")
             cnt += 1
+            sleep(0.1)
+        except KeyboardInterrupt:
+            print("Run Cmd Loop caught keyboardinterrupt")  # never caught here. just for demonstration purposes
+
+# スレッド・プロセスを起床するとともに、UIの制御を行う
+def displayLoop(exiting):
+    cnt = 0
+    while not exiting.is_set():
+        try:
+            print(f"{clock_gettime(0)}: disp cnt = {cnt}")
+            cnt += 1
             sleep(0.5)
         except KeyboardInterrupt:
-            print("caught keyboardinterrupt")  # never caught here. just for demonstration purposes
+            print("Display Loop caught keyboardinterrupt")  # never caught here. just for demonstration purposes
 
+# スレッド・プロセスを起床するとともに、UIの制御を行う
 def main():
     exiting = threading.Event()
     def signal_handler(signum, frame):
@@ -51,19 +65,25 @@ def main():
 
     signal.signal(signal.SIGTERM, signal_handler)
     with ThreadPoolExecutor() as executor:
-        executor.submit(runCtrlLoop, exiting)
         executor.submit(sensorLoop, exiting)
+        executor.submit(runCtrlLoop, exiting)
+        executor.submit(runCmdLoop, exiting)
+        executor.submit(displayLoop, exiting)
 
         try:
             uikeys = UIKey.Key()
             while not exiting.is_set():
                 keycmd = uikeys.detector()
                 if keycmd != UIKey.UISWCmd.NON_SW_EVNT:
-                    print(f"enqueue = {keycmd}")
-                    queue_man2machine.put_nowait(keycmd)
+                    if keycmd == UIKey.UISWCmd.EXIT_PUSH:
+                        print("!!! EXIT !!!")
+                        exiting.set()
+                    else:
+                        print(f"enqueue = {keycmd}")
+                        queue_man2machine.put_nowait(keycmd)
                 sleep(0.05)
         except KeyboardInterrupt:
-            print('Caught keyboardinterrupt')
+            print('Main Loop Caught keyboardinterrupt')
             exiting.set()
         finally:
             uikeys.close()
