5326eff3574caba2af13810132e99114fcf92eac
Imai <takap.colonel@gmail.com>
2024-09-03 14:22:32 +0900

    pigpio

--- IMI/libs/devices/driver/uisw/uisw.py
+++ IMI/libs/devices/driver/uisw/uisw.py
@@ -1,6 +1,7 @@
 import time
 from enum import Enum
 import RPi.GPIO as GPIO
+import pigpio
 
 # GPIO20 RAS2 SW0 
 # GPIO26 RAS1 SW1 
@@ -11,27 +12,30 @@ class UISW:
     SW1 = 26
     SW2 = 21
     __cnstcnt : int = 0
+    __pi:pigpio.pi
     def __init__(self) -> None:
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.SW0, GPIO.IN)
-        GPIO.setup(self.SW1, GPIO.IN)
-        GPIO.setup(self.SW2, GPIO.IN)
+        self.__pi =pigpio.pi()
+        self.__pi.set_mode(self.SW0,pigpio.INPUT)
+        self.__pi.set_mode(self.SW1,pigpio.INPUT)
+        self.__pi.set_mode(self.SW2,pigpio.INPUT)
+        self.__pi.set_pull_up_down(self.SW0, pigpio.PUD_UP)
+        self.__pi.set_pull_up_down(self.SW1, pigpio.PUD_UP)
+        self.__pi.set_pull_up_down(self.SW2, pigpio.PUD_UP)
+
         self.__cnstcnt += 1
     
     def __del__(self):
         if 0 < self.__cnstcnt:
-            GPIO.cleanup(self.SW0)
-            GPIO.cleanup(self.SW1)
-            GPIO.cleanup(self.SW2)
+            self.__pi.stop()
             self.__cnstcnt -= 1
     
     def close(self):
         self.__del__()
 
     def read(self, swittch : int) -> bool:
-        flag1 : bool = GPIO.input(swittch)
-        time.sleep(0.025)
-        flag2 : bool = GPIO.input(swittch)
+        flag1 : bool = bool(self.__pi.read(swittch))
+        time.sleep(0.01)
+        flag2 : bool = bool(self.__pi.read(swittch))
         if flag1 == False and flag2 == False :
             return True
         else :
